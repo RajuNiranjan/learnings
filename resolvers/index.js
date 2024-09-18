e751345135fc34669601fe5df94f93ace864f4bf
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-09-18 14:09:44 +0530

    connected to db

--- resolvers/index.js
+++
@@ -1,5 +0,0 @@
-import { mergeResolvers } from '@graphql-tools/merge'
-import { userResolver } from './user.resolver.js'
-import { transactionResolver } from './transaction.resolver.js'
-
-export const MergeResolvers = mergeResolvers([userResolver, transactionResolver])
