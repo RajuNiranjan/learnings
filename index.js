2eeca92894a094c68132171e7b5bdc69338eaa3f
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-09-18 11:00:59 +0530

    created passport.config.js

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from 'express'
+import http from 'http'
+import session from 'express-session'
+import connectMongo from 'connect-mongodb-session'
+import passport from 'passport'
+import { ApolloServer } from '@apollo/server'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import './db/connectDB.js'
+import { configurePassport } from './passport/passport.config.js'
+import { expressMiddleware } from '@apollo/server/express4'
+import { buildContext } from 'graphql-passport'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+
+dotenv.config()
+configurePassport()
+
+const app = express()
+
+const httpServer = http.createServer(app)
+
+const MongoStore = connectMongo(session)
+
+const store = new MongoStore({
+    uri: process.env.DB_URI,
+    collection: "session"
+})
+
+store.on("error", (e) => console.log(e))
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    },
+    store: store
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+})
+
+await server.start()
+
+app.use('/',
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true
+    }),
+    express.json(),
+    expressMiddleware(server, {
+        context: async (req, res) => buildContext({ req, res })
+    })
+)
+
+await new Promise((resolve) => ({ port: 4000 }, resolve))
+
+console.log(`server runnig at port 4000`);
