5aac59ed41a569c228ed059f11f893469643ce8f
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-09-18 11:40:56 +0530

    added prifilePIcture in user.resolver

--- resolvers/user.resolver.js
+++ resolvers/user.resolver.js
@@ -11,11 +11,14 @@ export const userResolver = {
                 if (existingUser) throw new Error("User already exists");
                 const salt = await bcrypt.genSalt(10);
                 const hashPassword = await bcrypt.hash(password, salt);
+                const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+                const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
                 const newUser = new UserModel({
                     username,
                     name,
                     gender,
-                    password: hashPassword
+                    password: hashPassword,
+                    profilePicture: gender === 'male' ? boyProfilePic : girlProfilePic
                 });
                 await newUser.save();
                 await context.login(newUser)
