a3dcf9e5913f0e07ab2bdbe7b500ade1a1eb8624
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-09-19 23:09:33 +0530

    ui created

---
+++ app/src/App.jsx
@@ -0,0 +1,22 @@
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import TransactionPage from "./pages/TransactionPage";
+import NotFound from "./pages/NotFound";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
