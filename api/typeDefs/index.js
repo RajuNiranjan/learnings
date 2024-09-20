6013b9574d54a4db878d4ceacc8b732124d40ee0
Raju <rajuniranjan1910@gmail.com>
2024-09-20 17:37:42 +0530

    initialized the express setup

--- api/typeDefs/index.js
+++
@@ -1,5 +0,0 @@
-import { mergeTypeDefs } from "@graphql-tools/merge"
-import { transactionTypeDef } from './transaction.typeDef.js'
-import { userTypeDef } from './user.typeDef.js'
-
-export const MergerTypeDefs = mergeTypeDefs([userTypeDef, transactionTypeDef])
