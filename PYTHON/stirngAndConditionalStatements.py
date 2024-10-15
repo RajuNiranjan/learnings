2c4b74e9492432b4169475a9908bcce6e23cbd72
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-10-15 21:20:14 +0530

    added pyton folders

---
+++ PYTHON/stirngAndConditionalStatements.py
@@ -0,0 +1,63 @@
+
+# STRINGS AND CONDITIONAL STATEMENTS////////////////////////////////////////
+
+# str = 'Niranjan Raju'
+
+# endsWith = str.endswith("u")
+# print("ENDS WITH -->",endsWith)
+
+# capitalize = str.capitalize()
+# print("CAPITALIZE -->",capitalize)
+
+# replace = str.replace("Niranjan","Tiruvalluru Nianjan")
+# print("REPLACE -->",replace)
+
+# find = str.find("Raju")
+# print("FIND -->", find)
+
+# count = str.count("a")
+# print("COUNT -->",count)
+
+
+# CONDITIONAL STATEMENTS ///////////////////////////////////////////////////
+
+# age = 22
+
+# if(age >= 21):
+#     print("can vote")
+# else:
+#   print("can't vote")
+
+
+# light = 'red'
+
+# if(light == 'red'):
+#     print("STOP")
+# elif(light == "orange"):
+#     print("READY TO GO")
+# else:
+#     print("GO")
+
+# marks = int(input("enter marks :"))
+
+# if(marks <= 100):
+#   if(marks <= 100 and marks >= 90):
+#       print("GRADE : A")
+#   elif(marks < 90 and marks >= 80):
+#       print("GRADE : B")
+#   elif(marks < 80 and marks >= 60):
+#       print("GRADE : C")
+#   elif(marks < 60 and marks >= 50):
+#       print("GRADE : D")
+#   else:
+#       print("FAIL")
+# else:
+#     print("Invalid Marks")
+
+
+# num = int(input("enter a number :"))
+
+# if(num % 2 == 0):
+#     print("EVEN")
+# else:
+#     print("ODD")
---
+++ PYTHON/variablesDataTypes.py
@@ -0,0 +1,29 @@
+
+# VARIABLES AND DATA TYPES //////////////////////////////////////////////////
+
+# print("Welcome to Python")
+
+# print(22)
+
+# print(22 + 1)
+
+# name = "Niranjan Raju"
+# age = 22
+# price = 31.18
+
+# print("name -->",name)
+# print("age -->",age)
+# print("price -->",price)
+
+# name = (input("enter name :"))
+# age = int(input("enter age :"))
+
+# print("hey there, I am",name,"I'm",age,'old')
+
+# PRACTICE ////////////////////////////////////////////////////////////////
+
+# val1 = int(input("enter value 1:"))
+# val2 = int(input("enter value 2:"))
+
+# print("Sum :",val1 + val2)
+
