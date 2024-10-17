8e9a05a5da24472bc1f3cdfeac68d5409a3b0659
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-10-17 06:55:41 +0530

    Dictionary and Sets

---
+++ PYTHON/dictionaryAndSet.py
@@ -0,0 +1,83 @@
+# DICTIONARY AND SET //////////////////////////////////////////////////////
+
+# DICTIONARY /////////////////////////////////////////////////////
+
+
+# info = {
+#   'name' : "John Deo",
+#   "subjects": ["Python","SQL","AWS","GraphQL","React Native"],
+#   "topics": ("Dictionaries","Sets"),
+#   "learning" : "coading",
+#   "age" : 22,
+# 'marks': 94.3,
+# }
+
+# # print(info)
+
+# print("KEYS -->", info.keys())
+# print("VALUES -->", info.values())
+# print("ITEMS -->", info.items())
+# print("GET -->", info.get("age"))
+# new_dict = {"city":"Hyderabad"}
+# info.update(new_dict)
+# print("UPDATE -->", info)
+
+
+# SET ////////////////////////////////////////////////////////////////////
+
+# collection = {1, 2, 3, 4,1,2,3, "raju","Niranjan","raju"}
+
+# print(collection)
+# print(type(collection))
+
+
+# collection = set()
+
+# collection.add(1)
+# collection.add(2)
+# collection.add(2)
+# collection.remove(1)
+# collection.add("John Deo")
+# collection.add((1, 2, 3))
+# collection.pop()
+
+# print(collection)
+
+
+
+# collection.clear()
+
+# print(collection)
+
+
+
+# set1 = {1,2,3}
+
+# set2 = {2,3,4}
+
+# print("UNINON -->", set1.union(set2))
+# print("INTERSECTION -->", set1.intersection(set2))
+
+
+# dictonary = {
+#   "cat" :"a small animal",
+#   "table" : ["a piece of furniture", "list of faccts & figures"]
+# }
+
+# print(dictonary)
+
+# sub = {"python","java","c++","python","javascript","java","python","java","c++","c"}
+
+# print(len(sub))
+
+# marks = {}
+
+# eng = int(input("enter eng marks: "))
+# math = int(input("enter math marks: "))
+# tel = int(input("enter tel marks: "))
+
+# marks.update({"eng" : eng})
+# marks.update({"math" : math})
+# marks.update({"tel" : tel})
+
+# print(marks)
