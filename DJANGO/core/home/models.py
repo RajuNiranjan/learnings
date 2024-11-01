8e925cd5fa842f08622fba6e10a41f8018fe04f1
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-11-01 05:35:23 +0530

    #PYTHON created models and migrated.

--- DJANGO/core/home/models.py
+++ DJANGO/core/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Model):
+  name = models.CharField(max_length=100)
+  age = models.PositiveIntegerField()
+  branch = models.CharField(max_length=10)
+  is_hostler = models.BooleanField(default=False)
+  city = models.CharField(max_length=50)
