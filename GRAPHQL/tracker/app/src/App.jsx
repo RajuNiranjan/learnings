4d1dd1ea137500f8da233491eabee42c0e08402b
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-11-13 12:45:56 +0530

    #GRAPHQL transaction

---
+++ GRAPHQL/tracker/app/src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import NotFound from "./pages/NotFoundPage";
+import TransactionPage from "./pages/TransactionPage";
+
+function App() {
+  const authUser = true;
+  return (
+    <>
+      {/* {authUser && <Header />} */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
+export default App;
