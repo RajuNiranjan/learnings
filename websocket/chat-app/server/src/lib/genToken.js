a332f046f1eb9aa5d81a0cd306ddb004cb05f8a9
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-12-20 15:37:22 +0530

    removed chat-app

--- websocket/chat-app/server/src/lib/genToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-import { ENV_VAR } from "./utils.js";
-
-export const genToken = (userId) => {
-  const payload = { userId };
-  return jwt.sign(payload, ENV_VAR.JWT_SECRET_KEY, {
-    expiresIn: ENV_VAR.JWT_EXPIRES_IN,
-  });
-};
--- websocket/chat-app/server/src/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { Login, LogOut, Register } from "../controllers/user.controller.js";
-
-export const AuthRoute = express.Router();
-
-AuthRoute.post("/register", Register);
-AuthRoute.post("/login", Login);
-AuthRoute.post("/logout", LogOut);
