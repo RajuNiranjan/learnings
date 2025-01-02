69c68a7d81b87dfaa1e88385da6a0f61d329f539
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-01-02 12:40:16 +0530

    added auth model route

---
+++ websocket/chat-app/server/src/routes/auth.route.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+import { protectRoute } from '../middleware/auth.middleware.js'
+import { checkAuth, login, logout, signUp, updateProfile } from '../controllers/auth.controller.js'
+
+export const AuthRouter = express.Router()
+
+AuthRouter.post("/signup", signUp)
+AuthRouter.post("/login", login)
+AuthRouter.post("/logout", logout)
+AuthRouter.put("/update-profile", protectRoute, updateProfile)
+AuthRouter.get("/check", protectRoute, checkAuth)
